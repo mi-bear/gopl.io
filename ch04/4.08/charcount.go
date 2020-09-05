995855d0b23a180e906e8013b41246fd19e6a2af
mi-bear <kumajet@gmail.com>
2020-09-05 17:19:40 +0900

    add #4 contents

---
+++ ch04/4.08/charcount.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)
+	types := make(map[string]int) // key: 文字種, value: count
+	var utflen [utf8.UTFMax + 1]int
+	invalid := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+
+		// TODO: 他の関数も利用する
+		// https://pkg.go.dev/unicode?tab=doc
+		if unicode.IsLetter(r) {
+			types["Letter"]++
+		}
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	fmt.Print("character type\tcount\n")
+	for t, n := range types {
+		fmt.Printf("%s\t%d\n", t, n)
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
